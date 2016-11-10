b0ecc9bdb0828f0a93af49ecd82bce02d223296f
rom <kbgb666@gmail.com>
2016-11-10 11:48:55 +0200

    'sd'

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,31 @@ router.get('/', function(req, res, next) {
 router.get('/rom', function(req, res, next) {
  //res.render("siksik");
 
- res.send({a:"hi"});;
+ pg.connect(process.env.DATABASE_URL,function(err,client){
+
+			var t= 'INSERT INTO C VALUES(1);'
+			client.query(t,function(err,reso)
+			{
+
+				res.end('done');
+
+
+
+			})
+});
+ router.get('/r', function(req, res, next) {
+ //res.render("siksik");
+
+ pg.connect(process.env.DATABASE_URL,function(err,client){
+
+			var t= 'SELECT * FROM C;'
+			client.query(t,function(err,reso)
+			{
+
+				res.end(RESO.ROWS);
+
+
+
+			})
 });
 module.exports = router;
