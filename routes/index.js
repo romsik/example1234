f736f5471febd482c2977f31f4aa31826fb53db9
rom <kbgb666@gmail.com>
2016-11-10 11:40:27 +0200

    'add'

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+	pg.connect(process.env.DATABASE_URL,function(err,client){
+
+			var t= 'CREATE TABLE C(ID INT);'
+			client.query(t,function(err,reso)
+			{
+
+				res.end('done');
+
+
+
+			})
+
+
+	})
+
+
+
 });
 router.get('/rom', function(req, res, next) {
  //res.render("siksik");
